635ce6733a964a918b315a20f3a677d1a52147ee
Thomas Olesen <thomasolesen0505@hotmail.com>
2024-03-19 12:01:24 +0100

    opdateret gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
 
         }
 
