b7885e3bb35a4d0650feee43f1144f235153ceb3
Thomas Olesen <thomasolesen0505@hotmail.com>
2024-03-19 11:59:33 +0100

    Deleted code

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
         }
-    }
+
 }
