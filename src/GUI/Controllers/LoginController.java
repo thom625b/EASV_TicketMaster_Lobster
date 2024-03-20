73a080276a67b9ce6c017ca4f071cf09bf220409
Jesper Nielsen <jesnie02@easv365.dk>
2024-03-20 10:00:48 +0100

    Login Side lavet

--- src/GUI/Controllers/LoginController.java
+++ src/GUI/Controllers/LoginController.java
@@ -1,33 +1,92 @@
 package GUI.Controllers;
 
-import GUI.Controllers.Frame.Admin.AdminFrameController;
-import GUI.Controllers.Frame.Coordinator.CoordinatorFrameController;
 import GUI.Model.UsersModel;
 import com.microsoft.sqlserver.jdbc.SQLServerException;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
-    @FXML
-    private Button SubmitBtn;
-    @FXML
-    private PasswordField passwordField;
-    @FXML
-    private TextField userId;
-    @FXML
-    private Label loginLabel;
 
     private UsersModel usersModel;
+    @FXML
+    private ImageView ImgLogin;
+
+    @FXML
+    private TextField adminMail;
+
+    @FXML
+    private PasswordField adminPassword;
+
+    @FXML
+    private BorderPane backBorder;
+
+    @FXML
+    private BorderPane borderInside;
+
+    @FXML
+    private Button btnSubmitAdmin;
+
+    @FXML
+    private Button btnSubmitEvent;
+
+    @FXML
+    private HBox hBoxBottom;
+
+    @FXML
+    private HBox hBoxTop;
+
+    @FXML
+    private Label lblAdmin;
+
+    @FXML
+    private Label lblLoginAdmin;
+
+    @FXML
+    private Label lblLoginEventCoordinator;
+
+    @FXML
+    private Label lblUser;
+
+    @FXML
+    private Label lblstatus;
+
+    @FXML
+    private Pane slidingPane;
+
+    @FXML
+    private StackPane stackImg;
+
+    @FXML
+    private Tab tabAdmin;
+
+    @FXML
+    private Tab tabEventCordinator;
+
+    @FXML
+    private TabPane tabPaneLogin;
+
+    @FXML
+    private TextField userMail;
+
+    @FXML
+    private PasswordField userPassword;
+
+    @FXML
+    private VBox vBoxLeft;
+
+    @FXML
+    private VBox vBoxRight;
 
 
     @Override
@@ -42,7 +101,7 @@ public class LoginController implements Initializable {
     }
 
 
-
+/*
     @FXML
     public void SubmitLoginPageAdmin(ActionEvent actionEvent) {
 
@@ -90,6 +149,33 @@ public class LoginController implements Initializable {
                 alert.showAndWait();
             }
         }
+
+ */
+
+
+
+    @FXML
+    void openAdminTab(Event event) {
+        TranslateTransition toLeftTransition = new TranslateTransition(new Duration(500), lblstatus);
+        toLeftTransition.setToX(slidingPane.getTranslateX());
+        toLeftTransition.play();
+        toLeftTransition.setOnFinished((ActionEvent event2) -> {
+            lblstatus.setText("ADMINISTRATOR");
+        });
+        tabPaneLogin.getSelectionModel().select(tabAdmin);
+    }
+
+    @FXML
+    void openUserTab(Event event) {
+        TranslateTransition toRightAnimation = new TranslateTransition(new Duration(500) ,lblstatus);
+        toRightAnimation.setToX(slidingPane.getTranslateX()+(slidingPane.getPrefWidth()-lblstatus.getPrefWidth()));
+        toRightAnimation.play();
+        toRightAnimation.setOnFinished((ActionEvent event1) -> {
+            lblstatus.setText("EVENT COORDINATOR");
+        });
+        tabPaneLogin.getSelectionModel().select(tabEventCordinator);
+    }
+        
     }
 
 
