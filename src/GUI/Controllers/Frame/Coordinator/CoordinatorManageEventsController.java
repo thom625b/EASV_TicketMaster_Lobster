f07f19f2debd834eee592eb58e46e1b3b78efcde
MadsFM <madsmoeller13@gmail.com>
2024-03-21 12:13:40 +0100

    lavet flow til coord vindue, med falske fxml filer

---
+++ src/GUI/Controllers/Frame/Coordinator/CoordinatorManageEventsController.java
@@ -0,0 +1,21 @@
+package GUI.Controllers.Frame.Coordinator;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+
+public class CoordinatorManageEventsController {
+
+
+    @FXML
+    private void goToHomePage(ActionEvent actionEvent) {
+       CoordinatorFrameController frameController = new CoordinatorFrameController();
+       frameController.goBack();
+    }
+
+    @FXML
+    private void mergeCoordAndEvent(ActionEvent actionEvent) {
+    }
+}
