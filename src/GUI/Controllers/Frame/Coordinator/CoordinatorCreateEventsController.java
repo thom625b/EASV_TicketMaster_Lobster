5d56c606a28a368793af19385a4d0f06985f0f5c
Jonas Speedtsberg <jonasspeedtsberg99@gmail.com>
2024-03-25 16:43:52 +0100

    Create Event page GUI

--- src/GUI/Controllers/Frame/Coordinator/CoordinatorCreateEventsController.java
+++ src/GUI/Controllers/Frame/Coordinator/CoordinatorCreateEventsController.java
@@ -1,6 +1,47 @@
 package GUI.Controllers.Frame.Coordinator;
 
-public class CoordinatorCreateEventsController {
+import GUI.Controllers.IController;
+import GUI.Model.UsersModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
+public class CoordinatorCreateEventsController implements IController {
 
+    @FXML
+    public TextField txtEventPicture;
+    @FXML
+    public TextField txtEventTitle;
+    @FXML
+    public TextField txtEventZipCode;
+    @FXML
+    public TextField txtEventCity;
+    @FXML
+    public TextField txtEventAddress;
+    @FXML
+    public Button btnUploadImageToEvent;
+    @FXML
+    public Button btnAddNewEvent;
+    @FXML
+    public DatePicker dpEventStartDate;
+
+    @FXML
+    public void EventStartDate(ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    public void UploadImageToEvent(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void AddNewEvent(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void setModel(UsersModel usersModel) {
+
+    }
 }
--- src/GUI/Controllers/Frame/Coordinator/CoordinatorEventPageController.java
+++ src/GUI/Controllers/Frame/Coordinator/CoordinatorEventPageController.java
@@ -1,42 +1,35 @@
 package GUI.Controllers.Frame.Coordinator;
 
-
 import GUI.Controllers.IController;
 import GUI.Model.UsersModel;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.io.IOException;
 import java.time.LocalDate;
 
 public class CoordinatorEventPageController implements IController {
-/*
-    @FXML
-    private TableView<Event_Mock> tblEventTable;
-    @FXML
-    private TableColumn<Event_Mock, String> tblEventTableCode;
-    @FXML
-    private TableColumn<Event_Mock, String> tblEventTableEventName;
-    @FXML
-    private TableColumn<Event_Mock, LocalDate> tblEventTableStartDate;
-    @FXML
-    private TableColumn<Event_Mock, String> tblEventTableStatus;
-    @FXML
-    private TableColumn<Event_Mock, Integer> tblEventTableDaysLeft;
-    @FXML
-    private TableColumn<Event_Mock, Integer> tblEventTableRegisteredParticipants;
-    @FXML
-    private TableColumn<Event_Mock, Void> editButton;
 
+    @FXML
+    private TableView<Event> tblEventTable;
+    @FXML
+    private TableColumn<Event, String> tblEventTableCode;
+    @FXML
+    private TableColumn<Event, String> tblEventTableEventName;
+    @FXML
+    private TableColumn<Event, LocalDate> tblEventTableStartDate;
+    @FXML
+    private TableColumn<Event, String> tblEventTableStatus;
+    @FXML
+    private TableColumn<Event, Integer> tblEventTableDaysLeft;
+    @FXML
+    private TableColumn<Event, Integer> tblEventTableRegisteredParticipants;
+    @FXML
+    private TableColumn<Event, Void> editButton;
 
- */
     @FXML
     public void goToCreateNewEvent(ActionEvent actionEvent) {
         CoordinatorFrameController.getInstance().loadPageCoordinatorCreateEventPage();
