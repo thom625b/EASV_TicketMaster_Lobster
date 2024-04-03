6385017200730515a43dbc8d1dff69a3d5b6169f
MadsFM <madsmoeller13@gmail.com>
2024-04-03 16:56:23 +0200

    oprettet resten af BE - har tastet det hele selv ;) NOT

--- src/BE/Tickets.java
+++ src/BE/Tickets.java
@@ -2,8 +2,32 @@ package BE;
 
 public class Tickets {
 
-    /*
-    Plads til Mads kan lave hans flotte settere og gettere samt konstruktør
+    private String pdf;
+    private int ticketID;
 
-    */
+
+    public Tickets(String pdf, int ticketID) {
+        this.pdf = pdf;
+        this.ticketID = ticketID;
+    }
+
+    public Tickets(String pdf) {
+        this.pdf = pdf;
+    }
+
+    private String getPdf() {
+        return pdf;
+    }
+
+    public void setPdf(String pdf) {
+        this.pdf = pdf;
+    }
+
+    public int getTicketID() {
+        return ticketID;
+    }
+
+    public void setTicketID(int ticketID) {
+        this.ticketID = ticketID;
+    }
 }
